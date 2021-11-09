c37302f61d07a1591e6dd72b37083b368e33ee34
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-09 09:33:53 +0800

    Add files via upload

---
+++ GunController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject TestObj;
+    public Transform FirePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FirePoint.position,FirePoint.forward , out hit))
+        {
+            TestObj.transform.position = hit.point;
+        }
+    }
+}
